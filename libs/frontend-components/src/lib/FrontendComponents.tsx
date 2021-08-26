acf427d93fd53f7cc94d2e588cb7a1142df5ddc6
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-08-26 15:32:04 -0300

    [chore] generated react component lib using react official generator plugin by nx

---
+++ libs/frontend-components/src/lib/FrontendComponents.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface FrontendComponentsProps {}
+
+export function FrontendComponents(props: FrontendComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to FrontendComponents!</h1>
+    </div>
+  );
+}
+
+export default FrontendComponents;
