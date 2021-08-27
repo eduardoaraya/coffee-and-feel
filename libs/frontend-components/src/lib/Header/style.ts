3b5bcf9c8db3fdff17060ea92c52326e092b2d83
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-08-27 14:23:45 -0300

    [demo] material styled sx props

--- libs/frontend-components/src/lib/Header/style.ts
+++ libs/frontend-components/src/lib/Header/style.ts
@@ -1,6 +1,10 @@
+import { SxProps } from '@material-ui/system';
+import { Theme } from '@material-ui/core';
+
 export default {
   header: {
     h: '77px',
     background: '#FFF',
+    color: (theme) => theme.palette.primary.main,
   },
-};
+} as SxProps<Theme>;
