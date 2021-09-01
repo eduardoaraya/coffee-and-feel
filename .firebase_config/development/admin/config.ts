d89944547a71b5ff67fdc7930435fb10c745e2a2
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-01 20:57:51 -0300

    [feature] created firebase config folder to properly separate firebase hosting configuration files amongst different projects

---
+++ .firebase_config/development/admin/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBkgJpUgX-iOGrERQt8qWgKK8b8e5NMgbM',
+  authDomain: 'coffee-and-feel.firebaseapp.com',
+  projectId: 'coffee-and-feel',
+  storageBucket: 'coffee-and-feel.appspot.com',
+  messagingSenderId: '201065708434',
+  appId: '1:201065708434:web:fd85206b4b931d121edf4b',
+  measurementId: 'G-E31YPDJL58',
+};
---
+++ .firebase_config/development/frontend/config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBkgJpUgX-iOGrERQt8qWgKK8b8e5NMgbM',
+  authDomain: 'coffee-and-feel.firebaseapp.com',
+  projectId: 'coffee-and-feel',
+  storageBucket: 'coffee-and-feel.appspot.com',
+  messagingSenderId: '201065708434',
+  appId: '1:201065708434:web:96877c7ebd18b0931edf4b',
+  measurementId: 'G-Y5TX7M4DPN',
+};
