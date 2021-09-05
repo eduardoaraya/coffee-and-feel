5d7ca065db2fdd0973a19f08bd5d4502d11eeaf2
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-05 01:29:46 -0300

    [feature][chore] initial swagger config and setup

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,14 +5,28 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Coffee And Feel')
+    .setDescription(
+      'Coffee and Feel project APIs to communicate with all required services'
+    )
+    .setVersion('1.0')
+    .addTag('microservice')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
