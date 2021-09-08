6ae7c0f7ff584a446abd10f6d28e5ef0e7f9778c
eduardoaraya <eduardojezine@gmail.com>
2021-09-08 09:34:32 -0300

    [feature] Add subscription details section

--- libs/frontend-pages/src/lib/HomePage/style.ts
+++ libs/frontend-pages/src/lib/HomePage/style.ts
@@ -232,4 +232,64 @@ export default {
   slideProductRepresentation: {
     padding: '35px 0px 105px 0px',
   },
+  subscriptionClubDetailsSection: {
+    padding: {
+      md: '105px 0px',
+      xs: '35px 20px',
+    },
+    '.title-section': {
+      textAlign: 'center',
+      maxWidth: '420px',
+      margin: '0 auto',
+      marginBottom: '85px',
+    },
+    '.subscription-details-wrapper': {
+      display: 'flex',
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'baseline',
+      flexWrap: 'wrap',
+
+      '.subscription-box-detail': {
+        minWidth: '315px',
+        margin: '10px 31px',
+        background: '#FFFFFF',
+        padding: '45px 20px',
+        borderRadius: '16.6px',
+        boxShadow: '0px 3.33333px 3.33333px 1.66667px rgba(137, 83, 227, 0.1)',
+        display: 'flex',
+        flexDirection: 'column',
+        '.title-box': {
+          padding: '0 36px',
+          'h3.MuiTypography-root': {
+            fontSize: '20px',
+            marginBottom: '14px',
+          },
+          'p.MuiTypography-root': {
+            fontSize: '14px',
+          },
+        },
+        a: {
+          color: (theme) => theme.palette.primary.main,
+          fontWeight: 'bolder',
+          fontSize: '14px',
+          textAlign: 'center',
+        },
+        '.check-list': {
+          padding: '36px',
+          li: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: '6px 0px',
+            svg: {
+              color: (theme) => theme.palette.success.main,
+            },
+            '>*': {
+              margin: '0 10px',
+            },
+          },
+        },
+      },
+    },
+  },
 } as HomePageStyle;
