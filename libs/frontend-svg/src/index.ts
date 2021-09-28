7eadb937c9b7a29b82b293f00ff54d48b468b773
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-28 01:05:49 -0300

    [enhancement][feature][chore] converted currently progressing section relevant SVG images to react components

--- libs/frontend-svg/src/index.ts
+++ libs/frontend-svg/src/index.ts
@@ -1,3 +1,5 @@
+export * from './lib/DiscountSVG/DiscountSVG';
+export * from './lib/DeliveryTimeSVG/DeliveryTimeSVG';
 export * from './lib/CoffeeBagSVG/CoffeeBagSVG';
 export * from './lib/CoffeeLeaf/CoffeeLeaf';
 export * from './lib/SubscriptionPageThirdSVG/SubscriptionPageThirdSVG';
