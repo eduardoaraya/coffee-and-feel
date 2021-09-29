0ce847400e9ff4ff5358c1fb755586c9cb70cd8e
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-29 04:59:43 -0300

    [feature] subscription benefits list card implemented

--- libs/frontend-components/src/index.ts
+++ libs/frontend-components/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/SubscriptionBenefitsListCard/SubscriptionBenefitsListCard';
 export * from './lib/SubscriptionItem/SubscriptionItem';
 export * from './lib/PaginationDots/PaginationDots';
 export * from './lib/SubscriptionAttributeCard/SubscriptionAttributeCard';
---
+++ libs/frontend-components/src/lib/SubscriptionBenefitsListCard/SubscriptionBenefitsListCard.stories.tsx
@@ -0,0 +1,44 @@
+import { Box } from '@material-ui/system';
+import { Story, Meta } from '@storybook/react';
+import {
+  SubscriptionBenefitsListCard,
+  SubscriptionBenefitsListCardProps,
+} from './SubscriptionBenefitsListCard';
+
+export default {
+  component: SubscriptionBenefitsListCard,
+  title: 'SubscriptionBenefitsListCard',
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'brazilDesktop1',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Box sx={{ p: '20px' }}>
+        <Story />
+      </Box>
+    ),
+  ],
+} as Meta;
+
+const Template: Story<SubscriptionBenefitsListCardProps> = (args) => (
+  <SubscriptionBenefitsListCard {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  title: 'Premium',
+  caption: 'Você recebe 60 cápsulas todos os meses',
+  price: 'R$ 132,60/mês',
+  benefits: [
+    'Café Blend Suave',
+    'Café Blend Intenso',
+    'Café Conillon',
+    'Desconto de 10% em toda loja',
+    'Café maceração carbônica',
+    '100% OFF no frete',
+    'Café Orgânico Biodinâmico',
+  ],
+};
