64035bc3797bc30c161ecd7354b97dc25f35e113
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-29 22:39:46 -0300

    [feature] full implementation for cookies dialog component

--- libs/frontend-components/src/lib/CookiesDialog/CookiesDialog.stories.tsx
+++ libs/frontend-components/src/lib/CookiesDialog/CookiesDialog.stories.tsx
@@ -1,9 +1,31 @@
+import { Box } from '@material-ui/system';
 import { Story, Meta } from '@storybook/react';
 import { CookiesDialog, CookiesDialogProps } from './CookiesDialog';
 
 export default {
   component: CookiesDialog,
   title: 'CookiesDialog',
+  parameters: {
+    layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'brazilDesktop1',
+    },
+  },
+  decorators: [
+    (Story) => (
+      <Box
+        sx={{
+          width: '100%',
+          height: '75vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Story />
+      </Box>
+    ),
+  ],
 } as Meta;
 
 const Template: Story<CookiesDialogProps> = (args) => (
@@ -11,4 +33,6 @@ const Template: Story<CookiesDialogProps> = (args) => (
 );
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  text: 'Seus dados estão em segurança e utilizamos cookies para fornecer uma experiência melhor durante a navegação.',
+};
--- libs/frontend-components/src/lib/CookiesDialog/CookiesDialog.tsx
+++ libs/frontend-components/src/lib/CookiesDialog/CookiesDialog.tsx
@@ -1,12 +1,134 @@
-/* eslint-disable-next-line */
-export interface CookiesDialogProps {}
+import { CookiesSVG } from '@atlascode/coffee-frontend-svg';
+import { AtlasStylesheet } from '@atlascode/coffee-shared-helpers';
+import {
+  Box,
+  BoxProps,
+  Button,
+  Typography,
+  IconButton,
+  ButtonProps,
+} from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 
-export function CookiesDialog(props: CookiesDialogProps) {
+/* eslint-disable-next-line */
+export interface CookiesDialogProps extends BoxProps {
+  text?: string;
+  PrivacyPolicyButtonProps?: ButtonProps;
+  ConfirmationButtonProps?: ButtonProps;
+  CloseIconButtonProps?: ButtonProps;
+}
+
+export function CookiesDialog({
+  sx,
+  text,
+  PrivacyPolicyButtonProps,
+  ConfirmationButtonProps,
+  CloseIconButtonProps,
+  ...rest
+}: CookiesDialogProps) {
   return (
-    <div>
-      <h1>Welcome to CookiesDialog!</h1>
-    </div>
+    <Box sx={{ ...styles.root, ...sx }} {...rest}>
+      <Box sx={styles.container}>
+        <Box sx={styles.header}>
+          <CookiesSVG sx={styles.illustration} />
+          <IconButton
+            {...CloseIconButtonProps}
+            sx={styles.closeButton}
+            size="small"
+          >
+            <Close fontSize="inherit" sx={styles.closeIcon} />
+          </IconButton>
+        </Box>
+
+        <Box sx={styles.textContainer}>
+          <Typography sx={styles.text}>{text}</Typography>
+        </Box>
+
+        <Box sx={styles.actionsContainer}>
+          <Button
+            {...PrivacyPolicyButtonProps}
+            variant="text"
+            color="secondary"
+            sx={styles.privacyLink}
+          >
+            Política de privacidade
+          </Button>
+          <Button
+            {...ConfirmationButtonProps}
+            variant="contained"
+            color="primary"
+            sx={styles.button}
+          >
+            Ok!
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
 export default CookiesDialog;
+
+const styles = AtlasStylesheet.create({
+  root: {
+    fontSize: '10px',
+    bgcolor: '#fff',
+    borderRadius: '17px',
+    boxShadow: (theme) => theme.shadows[3],
+    width: { xs: '25.8em', lg: '38.7em' },
+    minHeight: { xs: '25em', lg: '23.6em' },
+    position: 'relative',
+    p: { xs: '1.5em' },
+  },
+
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: { xs: '2em' },
+  },
+
+  closeButton: {},
+  closeIcon: {},
+
+  header: {
+    display: 'flex',
+    position: 'relative',
+    width: '100%',
+    justifyContent: 'flex-end',
+  },
+
+  illustration: {
+    position: 'absolute',
+    height: { xs: '8em' },
+    width: 'auto',
+    left: '50%',
+    transform: 'translateX(-50%) translateY(-50px)',
+  },
+
+  textContainer: {
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: { xs: '1.2em', lg: '1.4em' },
+    maxWidth: { xs: '23ch', lg: '39ch' },
+  },
+
+  actionsContainer: {
+    display: 'flex',
+    flexDirection: { xs: 'column' },
+    alignItems: 'center',
+    rowGap: { xs: '1.5em' },
+  },
+
+  privacyLink: {
+    fontSize: { xs: '1em', lg: '1.4em' },
+    color: (theme) => theme.palette.grey[400],
+    textTransform: 'inherit',
+  },
+  button: {
+    width: 'fit-content',
+  },
+});
