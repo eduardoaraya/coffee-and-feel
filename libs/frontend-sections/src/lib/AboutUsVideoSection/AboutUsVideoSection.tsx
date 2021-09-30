020444832b91fa919a754703c2196434d81d3a49
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-09-30 16:27:48 -0300

    [feature] about us video section

--- libs/frontend-sections/src/lib/AboutUsVideoSection/AboutUsVideoSection.tsx
+++ libs/frontend-sections/src/lib/AboutUsVideoSection/AboutUsVideoSection.tsx
@@ -1,27 +1,29 @@
 import { AtlasStylesheet } from '@atlascode/coffee-shared-helpers';
-import { Box, BoxProps } from '@material-ui/core';
+import { Box, BoxProps, Container, Typography } from '@material-ui/core';
+import AboutUsVideoPlayer from './AboutUsVideoPlayer';
 
 /* eslint-disable-next-line */
-export interface AboutUsVideoSectionProps extends BoxProps<'iframe'> {}
+export interface AboutUsVideoSectionProps extends BoxProps {}
 
-export function AboutUsVideoSection({
-  sx,
-  src = 'https://www.youtube.com/embed/1qfjLCOXMMg?controls=0',
-  ...rest
-}: AboutUsVideoSectionProps) {
+export function AboutUsVideoSection({ sx, ...rest }: AboutUsVideoSectionProps) {
   return (
-    <Box
-      sx={{ ...styles.root, ...sx }}
-      {...rest}
-      component="iframe"
-      width="560"
-      height="315"
-      src={src}
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-    />
+    <Box sx={{ ...styles.root, ...rest }}>
+      <Container sx={styles.container}>
+        <Box sx={styles.textContainer}>
+          <Typography sx={styles.title}>Café com sentimento</Typography>
+          <Typography sx={styles.caption}>
+            Do pequeno produtor para o mundo, esse é o nosso lema, comunicar por
+            inteiro o potencial que possui a bebida queridinha dos brasileiros.
+            A concepção da nossa marca leva você a sair do automático. Que tal
+            uma viagem pelos cafezais?
+          </Typography>
+        </Box>
+
+        <Box sx={styles.videoContainer}>
+          <AboutUsVideoPlayer />
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
@@ -30,7 +32,38 @@ export default AboutUsVideoSection;
 const styles = AtlasStylesheet.create({
   root: {
     fontSize: '10px',
-    width: { xs: '30em', lg: '83.17em' },
-    height: { xs: '16.7em', lg: '46.2em' },
+  },
+
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: { xs: 'center' },
+    rowGap: { xs: '4em' },
+  },
+
+  textContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+    rowGap: { xs: '1.5em' },
+  },
+
+  videoContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
+
+  title: {
+    fontSize: { xs: '2em', lg: '4em' },
+    color: '#323232',
+    fontWeight: 700,
+  },
+
+  caption: {
+    fontSize: { xs: '1.6em' },
+    color: (theme) => theme.palette.grey[600],
+    maxWidth: { xs: '78ch' },
   },
 });
