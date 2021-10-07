c34685d348a6f998c013c93683dbf42c53ad0661
Aleksander Mirowsky <alex.xande10@gmail.com>
2021-10-07 03:19:28 -0300

    [feature] checkout subscription coffee selection screen component implementantion

--- libs/frontend-components/src/lib/Checkout/CheckoutLayout/CheckoutSubscriptionCoffee/CheckoutSubscriptionCoffee.stories.tsx
+++ libs/frontend-components/src/lib/Checkout/CheckoutLayout/CheckoutSubscriptionCoffee/CheckoutSubscriptionCoffee.stories.tsx
@@ -2,11 +2,14 @@ import { Story, Meta } from '@storybook/react';
 import {
   CheckoutSubscriptionCoffee,
   CheckoutSubscriptionCoffeeProps,
+  SlotList,
 } from './CheckoutSubscriptionCoffee';
+import { TextField, MenuItem, TextFieldProps } from '@material-ui/core';
+import React from 'react';
 
 export default {
   component: CheckoutSubscriptionCoffee,
-  title: 'CheckoutSubscriptionCoffee',
+  title: 'Checkout/Checkout Layout/CheckoutSubscriptionCoffee',
   parameters: {
     layout: 'padded',
     viewport: {
@@ -19,5 +22,45 @@ const Template: Story<CheckoutSubscriptionCoffeeProps> = (args) => (
   <CheckoutSubscriptionCoffee {...args} />
 );
 
+const coffeSelectList = [
+  <MenuItem value="Café 1"> Café 1 </MenuItem>,
+  <MenuItem value="Café 2"> Café 2 </MenuItem>,
+  <MenuItem value="Café 3"> Café 3 </MenuItem>,
+  <MenuItem value="Café 4"> Café 4 </MenuItem>,
+];
+
+const coffeListProps: SlotList = {
+  label: 'Café especiais 1',
+  slots: [
+    {
+      select: true,
+      label: 'Selecione seus cafés',
+      children: coffeSelectList,
+    },
+    {
+      select: true,
+      label: 'Selecione seus cafés',
+      children: coffeSelectList,
+    },
+    {
+      select: true,
+      label: 'Selecione seus cafés',
+      children: coffeSelectList,
+    },
+    {
+      select: true,
+      label: 'Selecione seus cafés',
+      children: coffeSelectList,
+    },
+  ],
+};
+
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  title: 'Você receberá 10 cápsulas de cada café selecionado',
+  list: [
+    { ...coffeListProps },
+    { ...coffeListProps, label: 'Cafés especiais 2' },
+    { ...coffeListProps, label: 'Cafés especiais 3' },
+  ],
+};
--- libs/frontend-components/src/lib/Checkout/CheckoutLayout/CheckoutSubscriptionCoffee/CheckoutSubscriptionCoffee.tsx
+++ libs/frontend-components/src/lib/Checkout/CheckoutLayout/CheckoutSubscriptionCoffee/CheckoutSubscriptionCoffee.tsx
@@ -1,4 +1,11 @@
-import { Box } from '@material-ui/core';
+import {
+  Box,
+  TextField,
+  TextFieldProps,
+  Typography,
+  Button,
+  ButtonProps,
+} from '@material-ui/core';
 import {
   AtlasJSSShallow,
   AtlasStylesheet,
@@ -6,21 +13,114 @@ import {
 import _ from 'lodash';
 import React from 'react';
 
-/* eslint-disable-next-line */
+export type SlotList = {
+  label: string;
+  slots: TextFieldProps[];
+};
+
 export interface CheckoutSubscriptionCoffeeProps {
   JSS?: AtlasJSSShallow<typeof stylesheet>;
+  title?: string;
+  list?: SlotList[];
+  ForwardButtonProps?: ButtonProps;
+  BackwardButtonProps?: ButtonProps;
 }
 
 export function CheckoutSubscriptionCoffee({
   JSS,
+  title = 'Placeholder section title',
+  list = [],
+  ForwardButtonProps,
+  BackwardButtonProps,
 }: CheckoutSubscriptionCoffeeProps) {
   const styles = React.useMemo(() => _.merge(stylesheet, JSS), [JSS]);
 
-  return <Box sx={styles.root}></Box>;
+  return (
+    <Box sx={styles.root}>
+      <Typography sx={styles.title} color="secondary">
+        {title}
+      </Typography>
+
+      <Box sx={styles.categoryGrid}>
+        {list.map(({ label, slots }, index) => {
+          return (
+            <Box sx={styles.slotsContainer} key={index}>
+              <Typography sx={styles.slotTitle}>{label}</Typography>
+              {slots.map((valueInner, indexInner) => {
+                return <TextField {...valueInner} key={indexInner} />;
+              })}
+            </Box>
+          );
+        })}
+      </Box>
+
+      <Box sx={styles.buttonsContainer}>
+        <Button
+          {...{
+            ...BackwardButtonProps,
+            variant: 'outlined',
+            color: 'secondary',
+            children: 'Voltar',
+            sx: styles.backwardButton,
+          }}
+        />
+        <Button
+          {...{
+            ...ForwardButtonProps,
+            variant: 'contained',
+            color: 'success',
+            children: 'Avançar',
+            sx: styles.forwardButton,
+          }}
+        />
+      </Box>
+    </Box>
+  );
 }
 
 export default CheckoutSubscriptionCoffee;
 
 const stylesheet = AtlasStylesheet.create({
-  root: {},
+  root: {
+    fontSize: '10px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: { xs: '3em' },
+  },
+
+  title: {
+    fontSize: { xs: '1.6em', lg: '2em' },
+  },
+
+  slotsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: { xs: '1.75em' },
+  },
+
+  slotTitle: {
+    fontSize: { xs: '1.4em', lg: '1.6em' },
+    fontWeight: 800,
+  },
+
+  slotItem: {},
+
+  categoryGrid: {
+    display: 'grid',
+    gridTemplateColumns: { xs: '1fr', lg: '0.5fr 0.5fr' },
+    gridAutoFlow: 'row',
+    gap: { xs: '3em' },
+    rowGap: { xs: '5em' },
+  },
+
+  buttonsContainer: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: { xs: 'center', lg: 'flex-end' },
+    gap: { xs: '4em' },
+  },
+
+  forwardButton: {},
+  backwardButton: {},
 });
